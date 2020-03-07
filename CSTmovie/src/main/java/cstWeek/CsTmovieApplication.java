d848d1f0207f54cfe5f19db5a5679bc741a84615
rices <rices@172.31.98.147>
2020-03-07 14:17:51 -0800

    first from eclipse

---
+++ CSTmovie/src/main/java/cstWeek/CsTmovieApplication.java
@@ -0,0 +1,13 @@
+package cstWeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsTmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsTmovieApplication.class, args);
+	}
+
+}
